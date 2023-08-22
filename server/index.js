ffc5015b926dcd6e2b9b586ef3eea6c14d4d3f15
Rafael Souza <rafaelszcardoso@gmail.com>
2023-08-22 09:55:34 -0300

    server rate limits, .ignore

--- server/index.js
+++ server/index.js
@@ -1,20 +1,48 @@
 // index.js
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
+const responseTime = require("response-time");
+const rateLimit = require("express-rate-limit");
+const { upload } = require("./upload/upload");
+const { initializeApp } = require("firebase-admin/app");
+var admin = require("firebase-admin");
+const { getStorage } = require("firebase-admin/storage");
+const app = express();
+var serviceAccount = require("./config.json");
 
-const app = express()
-const PORT = 4000
+
+const limiter = rateLimit({
+  windowMs: 20 * 60 * 1000, // 20 minutes
+  max: 400, // Limit each IP to 100 requests per `window` (here, per 20 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(cors());
+
+app.use(responseTime());
+app.use(
+  rateLimit({
+    windowMs: 1 * 60 * 60 * 1000, // 12 hour duration in milliseconds
+    max: 600,
+    message: "You exceeded requests hour limit!",
+    headers: true,
+  })
+);
+
+const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT} `);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+app.post("/upload", (req, res) => {
+  upload(req, res, bucket);
+});
 
 // Export the Express API
-module.exports = app
+module.exports = app;
---
+++ server/upload/upload.js
@@ -0,0 +1,70 @@
+const Busboy = require('busboy');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const { getStorage, getDownloadURL, ref} = require('firebase-admin/storage');
+const UUID = require("uuid-v4");
+
+function upload(req, res, bucket){
+  const busboy = new Busboy({ headers: req.headers });
+    const tmpdir = os.tmpdir();
+    const fields = {};
+    const uploads = {};
+ 
+    busboy.on('field', (fieldname, val) => {
+        console.log(`Processed field ${fieldname}: ${val}.`);
+        fields[fieldname] = val;
+    });
+ 
+    const fileWrites = [];
+ 
+    // This code will process each file uploaded.
+    busboy.on('file', (fieldname, file, filename) => {
+        //console.log(`Processed file ${filename}`);
+        const filepath = path.join(tmpdir, filename);
+        uploads[fieldname] = filepath;
+ 
+        const writeStream = fs.createWriteStream(filepath);
+        file.pipe(writeStream);
+ 
+        const promise = new Promise((resolve, reject) => {
+            file.on('end', () => {
+                writeStream.end();
+            });
+            writeStream.on('finish', resolve);
+            writeStream.on('error', reject);
+        });
+        fileWrites.push(promise);
+    });
+ 
+    busboy.on('finish', async () => {
+        var url;
+        await Promise.all(fileWrites);
+        for (const file in uploads) {
+            //console.log(uploads[file]);
+            let uuid = UUID();
+            let options = {
+                metadata: {
+                    metadata: {
+                        firebaseStorageDownloadTokens: uuid,
+                        cacheControl: 'public,max-age=12600'
+                    }
+                }
+            };
+
+			const path = bucket.upload(uploads[file], options)
+            path.then(result=>{
+				bucket_url = result[0]['metadata']['bucket']
+				file_name = result[0]['metadata']['name']
+                url = "https://firebasestorage.googleapis.com/v0/b/"+bucket_url+"/o/"+file_name+"?alt=media&token="+uuid
+                res.send({"url":url});
+			})
+            //fs.unlinkSync(uploads[file]);
+ 
+        }
+    });
+ 
+    busboy.end(req.rawBody);
+}
+
+module.exports = {upload}
