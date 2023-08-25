ae423afe3a929bb2f20f8a01cffb08b94cdc315a
Rafael Souza <rafaelszcardoso@gmail.com>
2023-08-25 20:39:48 -0300

    try to fix cors

--- server/api/get-user.js
+++ server/api/get-user.js
@@ -1,11 +1,11 @@
 const { db } = require("../config/firebase");
 const cors = require("../config/cors").default;
 
-module.exports = async (req, res) => {
+module.exports = cors(  async (req, res) => {
   res.setHeader('Cache-Control', 's-maxage=86400');
     var wallet = req.body.wallet;
     var ref = db.ref("/users/" + wallet);
     ref.once("value", function (snapshot) {
       res.json(snapshot.val());
     });
-  };
+  });
