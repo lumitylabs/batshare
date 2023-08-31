a9c20d17a7272df4bcd87c7ddeb218079f1040f7
Rafael Souza <rafaelszcardoso@gmail.com>
2023-08-30 21:30:42 -0300

    test withdraw

--- server/api/withdraw.js
+++ server/api/withdraw.js
@@ -5,6 +5,8 @@ module.exports = cors(async (req, res) => {
   res.setHeader("Cache-Control", "s-maxage=86400");
   console.log(req)
   console.log(req.body);
+  console.log(req.body.root[0].txs)
+  console.log(Object.keys(req.body));
 
   res.json({ msg: "success" });
 });
