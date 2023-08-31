8113c417c9032dd3c43f3b9c0424371d9eda3af7
Rafael Souza <rafaelszcardoso@gmail.com>
2023-08-30 21:36:47 -0300

    test withdraw

--- server/api/withdraw.js
+++ server/api/withdraw.js
@@ -3,10 +3,9 @@ const cors = require("../config/cors").default;
 
 module.exports = cors(async (req, res) => {
   res.setHeader("Cache-Control", "s-maxage=86400");
-  console.log(req)
   console.log(req.body);
-  console.log(req.body.root[0].txs)
   console.log(Object.keys(req.body));
+  console.log(req.body.root[0].txs)
 
   res.json({ msg: "success" });
 });
